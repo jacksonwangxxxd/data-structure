e87a8e3f91a548e5fee46acd2023f158ed03bd49
piyan269 <149127417+piyan269@users.noreply.github.com>
2024-01-11 20:11:14 +0800

    Update MyWordCloud.java

--- WordCloud/MyWordCloud.java
+++ WordCloud/MyWordCloud.java
@@ -4,6 +4,7 @@ import com.kennycason.kumo.WordFrequency;
 import com.kennycason.kumo.bg.CircleBackground;
 import com.kennycason.kumo.font.KumoFont;
 import com.kennycason.kumo.font.scale.LinearFontScalar;
+import com.kennycason.kumo.image.AngleGenerator;
 import com.kennycason.kumo.palette.ColorPalette;
 
 import java.awt.*;
@@ -21,21 +22,26 @@ public class MyWordCloud {
     // 產生的文字雲png輸出位置
     static String outputPath = "./wordcloud.png";
 
-    /* mySql指令
+    static PreparedStatement pstmtInsertOrUpdate = null;
 
-    create database `word`;
-    create table `count_word`(
-	    `word` varchar(20),
-        `frequency` INT
-    );
 
-    insert into `count_word` values ('特斯拉', 9);  特斯拉出現9次
-    
-    */
+    /*  建立資料庫SQL語法
+        create database `word`;
+        use `word`;
 
-    public static void main(String[] args) {
+        create table `count_search`(
+	        `word` varchar(20) unique,
+            `count` INT default 1
+        );
+     */
 
+    public static void generateWordCloud(String inputText){
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
+
+            String insertOrUpdateSQL = "INSERT INTO count_search (word, count) VALUES (?, 1) ON DUPLICATE KEY UPDATE count = count + 1";
+            pstmtInsertOrUpdate = connection.prepareStatement(insertOrUpdateSQL);
+            pstmtInsertOrUpdate.setString(1, inputText);
+            pstmtInsertOrUpdate.executeUpdate();
 
             List<WordFrequency> wordFrequencies = getDataFromDatabase(connection);
             generateWordCloud(wordFrequencies);
@@ -48,14 +54,14 @@ public class MyWordCloud {
     private static List<WordFrequency> getDataFromDatabase(Connection connection) throws SQLException {
         List<WordFrequency> wordFrequencies = new ArrayList<>();
 
-        String query = "SELECT word, frequency FROM count_word";
+        String query = "SELECT word, count FROM count_search";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
 
             while (resultSet.next()) {
                 
                 String word = resultSet.getString("word");
-                int frequency = resultSet.getInt("frequency");
+                int frequency = resultSet.getInt("count");
 
                 wordFrequencies.add(new WordFrequency(word, frequency));
             }
@@ -70,9 +76,11 @@ public class MyWordCloud {
         WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
         wordCloud.setPadding(2);
         wordCloud.setBackground(new CircleBackground(300));
-        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1)));
-        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
+        wordCloud.setBackgroundColor(Color.WHITE);
+        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
+        wordCloud.setFontScalar(new LinearFontScalar(20, 100));
         wordCloud.setKumoFont(new KumoFont(new Font("SimSun", Font.PLAIN, 1)));
+        wordCloud.setAngleGenerator(new AngleGenerator(0));
         wordCloud.build(wordFrequencies);
 
         try {
